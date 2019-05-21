fcf3f3f9fe9f13f911405179eb53c745e5aeb589
Anson <nguyenanson04101997@gmail.com>
2019-05-21 19:26:30 +0700

    11

---
+++ index3.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+<body>
+<div>
+<h1>chỉ số bmi</h1>
+<form name="bmi" action="" method="post">
+    <div class="form-group">
+        <label>Chiều cao</label>
+        <input type="text" class="form-control" name="height" value="">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+    <div class="form-group">
+        <label>cân nặng</label>
+        <input type="text" class="form-control" name="weight">
+
+    <button type="submit" name="calc" class="btn btn-primary">Submit</button>
+</form>
+</div>
+</body>
+</html>
